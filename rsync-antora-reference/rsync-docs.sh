071288197151bf8e4f7bd896d81303c0fcdf242a
Rob Winch <rwinch@users.noreply.github.com>
2023-06-11 22:57:49 -0500

    Support configuring site-path
    Closes gh-2

--- rsync-antora-reference/rsync-docs.sh
+++ rsync-antora-reference/rsync-docs.sh
@@ -5,12 +5,12 @@ SSH_HOST_PATH="$2"
 SSH_PRIVATE_KEY="$3"
 SSH_KNOWN_HOST="$4"
 DRY_RUN="$5"
+FROM="$6"
 
-FROM=build/site
 SSH_PRIVATE_KEY_PATH="$HOME/.ssh/${GITHUB_REPOSITORY:-publish-docs}"
 
-if [ "$#" -ne 5 ]; then
-  echo -e "not enough arguments USAGE:\n\n$0 \$SSH_HOST \$SSH_HOST_PATH \$SSH_PRIVATE_KEY \$SSH_KNOWN_HOST \$DRY_RUN\n\n" >&2
+if [ "$#" -ne 6 ]; then
+  echo -e "not enough arguments USAGE:\n\n$0 \$SSH_HOST \$SSH_HOST_PATH \$SSH_PRIVATE_KEY \$SSH_KNOWN_HOST \$DRY_RUN \$DOC_PATH\n\n" >&2
   exit 1
 fi
 if [ -z "$SSH_HOST" ]; then
